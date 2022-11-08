941a422684116473e8835482be04b50734dc9eee
valentinusedward <valentinusedward27@gmail.com>
2022-11-08 16:43:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd10_a_10571/MahasiswaData.kt
@@ -0,0 +1,12 @@
+package com.example.gd10_a_10571
+
+import com.google.gson.annotations.SerializedName
+
+data class MahasiswaData(
+    @SerializedName("mhsnobp") val nim:String,
+    @SerializedName("mhsnama") val nama:String,
+    @SerializedName("mhsalamat") val alamat:String,
+    @SerializedName("prodinama") val prodi:String,
+    @SerializedName("mhstgllhr") val tgllhr:String,
+)
+
